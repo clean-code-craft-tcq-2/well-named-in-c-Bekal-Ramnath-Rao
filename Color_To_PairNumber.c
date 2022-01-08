7e2dd65b947eb4ed1d965824d4b3759237380343
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-01-08 19:40:21 +0530

    Delete Color_To_PairNumber.c

--- Color_To_PairNumber.c
+++
@@ -1,6 +0,0 @@
-#include "Color_To_PairNumber.h"
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
-}
